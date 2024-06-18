5458dd577a31dc1d1fb5e54ce39d1cedff371c57
binaro <binaro97@gmail.com>
2024-06-18 12:19:58 +0300

    start front

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json()); 
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -15,6 +18,17 @@ app.use((req, res, next)=>{
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-});
+app.use((error, req, res, next)=>{
+    console.log(error);
+    const status = error.statusCode
+    const message = error.message
+    res.status(status).json({message:message})
+})
+
+mongoose.connect('mongodb+srv://binaro97:7kYHbTN6C3aYrZMr@cluster0.4v5prio.mongodb.net/feed?retryWrites=true&w=majority&appName=Cluster0')
+.then(result=>{
+    app.listen(8080, () => {
+        console.log('Server is running on port 8080');
+    });
+})
+.catch(err=>console.log(err))
